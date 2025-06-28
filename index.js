0bc54966a6db22eb39aedbb564d297dec406ef1c
c4ntini <115190057+c4ntini@users.noreply.github.com>
2025-06-27 23:53:21 -0300

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const { Configuration, OpenAIApi } = require('openai');
